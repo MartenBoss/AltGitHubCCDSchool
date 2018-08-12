ee9fdbf9271f96c59fcd2922798c8029ad71577e
Marten Kabelitz <martenkabel@gmail.com>
2018-08-12 11:38:25 +0200

    Russische BauernMulti ohne Usereingabe

---
+++ RussischeBauernMulti/RussischeBauernMulti/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RussischeBauernMulti
+{
+    class Program
+    {
+        public static int RussBauerMultiplikation(int faktor1, int faktor2)
+        {
+            int ergebnis = 0;
+            if (faktor1 % 2 != 0)
+                ergebnis = faktor2;
+            while(faktor1 > 1)
+            {
+                faktor1 /= 2;
+                faktor2 *= 2;
+                if (faktor1 % 2 != 0)
+                    ergebnis += faktor2;
+            }
+            return ergebnis;
+        }
+
+        static void Main(string[] args)
+        {
+            int x = RussBauerMultiplikation(47, 42);
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
